29e7cd9028e290548a9f0efe5a9eacb93ec681bb
Elie <reyaraelie0@gmail.com>
2025-02-05 14:29:15 +0000

    Deplacement class UserController dans model authModel precedemment appele user

--- controllers/authController.php
+++ controllers/authController.php
@@ -4,41 +4,6 @@
 require_once __DIR__.'/../config/database.php'; // Assurez-vous que la connexion est bien établie
 require_once __DIR__.'/../models/user.php';
 
-class UserController {
-    private $conn;
-
-    // Constructeur avec la connexion à la base de données
-    public function __construct($conn) {
-        $this->conn = $conn;
-    }
-
-    // Méthode pour connecter un utilisateur
-    public function login($username, $password) {
-        $user = new User($this->conn, $username, $password);
-
-        $authenticated_user = $user->login();
-        if ($authenticated_user) {
-            session_start();
-            $_SESSION['user_id'] = $authenticated_user['id'];
-            $_SESSION['username'] = $authenticated_user['username'];
-            $_SESSION['role'] = $authenticated_user['role'];
-            return true;
-        } else {
-            return false; // Échec de la connexion
-        }
-    }
-
-    // Méthode pour récupérer les informations de l'utilisateur connecté
-    public function dashboard() {
-        session_start();
-        if (isset($_SESSION['user_id'])) {
-            return $_SESSION; // Retourne les infos de session
-        } else {
-            return false; // L'utilisateur n'est pas connecté
-        }
-    }
-}
-
 // Vérification des requêtes POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $action = isset($_GET['action']) ? $_GET['action'] : '';
